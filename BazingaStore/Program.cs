862b96311017215ff146a54dbda7a31315a85fa9
Thiago Mazzi <thiagodev728@gmail.com>
2025-02-12 09:22:21 -0300

    Adicionando Data e builder

--- BazingaStore/Program.cs
+++ BazingaStore/Program.cs
@@ -1,6 +1,25 @@
+using BazingaStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
