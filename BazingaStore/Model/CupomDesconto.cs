7fd481c7b0922f35ff149abe6e096f9e29fd215a
Rattini <rattinicursods@gmail.com>
2025-02-12 14:51:20 -0300

    Adição da classe CarrinhoItem, CupomDesconto e ItemVenda

---
+++ BazingaStore/Model/CupomDesconto.cs
@@ -0,0 +1,10 @@
+﻿namespace BazingaStore.Model
+{
+    public class CupomDesconto
+    {
+        public Guid CupomDescontoId { get; set; }
+        public string Codigo { get; set; }
+        public decimal PercentualDesconto { get; set; }
+        public DateTime DataValidade { get; set; }
+    }
+}
