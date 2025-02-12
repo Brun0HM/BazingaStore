5a9eb247d44789c6298d72cff7a6e08645d14752
Thiago Mazzi <thiagodev728@gmail.com>
2025-02-12 14:53:06 -0300

    Models Avaliação

--- BazingaStore/Model/Carrinho.cs
+++ BazingaStore/Model/Carrinho.cs
@@ -4,7 +4,6 @@
     {
         public Guid CarrinhoId { get; set; }
         public Guid UsuarioId { get; set; }
-        public DateTime? DataCriacao { get; set; }
         public decimal Total { get; set; } = 0;
         public bool Finalizado { get; set; } = false;
 
