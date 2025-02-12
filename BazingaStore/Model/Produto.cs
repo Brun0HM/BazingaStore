c3bb34b110da103f882dd69fdf5abe8ac6cfd56e
Thiago Mazzi <thiagodev728@gmail.com>
2025-02-12 11:00:00 -0300

    Criação das models Carrinho Categoria e Produtos

--- BazingaStore/Model/Produto.cs
+++ BazingaStore/Model/Produto.cs
@@ -2,5 +2,14 @@
 {
     public class Produto
     {
+        public Guid ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? Imagem { get; set; }
+        public int Estoque { get; set; }
+        public Guid CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+
     }
 }
