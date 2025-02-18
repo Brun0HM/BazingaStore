f4ff87671452ea937b62ef521bcfe0d9e6fd4275
Thiago Mazzi <thiagodev728@gmail.com>
2025-02-18 09:45:51 -0300

    Alterando métodos do Carrinho

--- BazingaStore/Migrations/20250218120410_Atualizar.Designer.cs
+++ BazingaStore/Migrations/20250218120410_Atualizar.Designer.cs
@@ -4,6 +4,7 @@ using BazingaStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BazingaStore.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250218120410_Atualizar")]
+    partial class Atualizar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
