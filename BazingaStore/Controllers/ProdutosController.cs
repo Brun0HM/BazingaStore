c70b9e1f308437bcc4b106f95ccc7f38734200b3
Brun0 <brunodev41@gmail.com>
2025-06-10 08:44:48 -0300

    Revert "Melhorias na funcionalidade de avaliações de produtos"
    This reverts commit caf5be68803f929929bb3f9ae54a0bf29b098415.

--- BazingaStore/Controllers/ProdutosController.cs
+++ BazingaStore/Controllers/ProdutosController.cs
@@ -22,25 +22,11 @@ namespace BazingaStore.Controllers
         }
 
         // GET: api/Produtos
-        // Controllers/ProdutosController.cs
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Produto>>> GetProduto()
         {
-            var produtos = await _context.Produto.ToListAsync();
-
-            // Busca as médias das avaliações para cada produto
-            foreach (var produto in produtos)
-            {
-                var avaliacoes = await _context.Avaliacao
-                    .Where(a => a.ProdutoId == produto.ProdutoId)
-                    .ToListAsync();
-
-                produto.MediaAvaliacao = avaliacoes.Any() ? avaliacoes.Average(a => a.Nota) : null;
-            }
-
-            return produtos;
+            return await _context.Produto.ToListAsync();
         }
-
 
         // GET: api/Produtos/5
         [HttpGet("{id}")]
--- BazingaStore/Model/Produto.cs
+++ BazingaStore/Model/Produto.cs
@@ -1,19 +1,15 @@
-﻿// Model/Produto.cs
-namespace BazingaStore.Model
+﻿namespace BazingaStore.Model
 {
     public class Produto
     {
-        public Guid ProdutoId { get; set; }
+        public Guid ProdutoId { get; set; } // deixa o banco gerar
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string? Imagem { get; set; }
         public int Estoque { get; set; }
+
         public Guid CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
-
-        // Propriedade não mapeada para exibir a média das avaliações
-        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-        public double? MediaAvaliacao { get; set; }
     }
 }
